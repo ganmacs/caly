03aaf204035a770283cbcf185e15300218d7d836
ganmacs <ganmacs@gmail.com>
2015-05-04 10:47:18 +0900

    fix typo

--- lib/env.rb
+++ lib/env.rb
@@ -3,7 +3,7 @@ require 'Forwardable'
 module Caly
   class Env
     extend Forwardable
-    def_delegators :@env, :[]=, []
+    def_delegators :@env, :[]=, :[]
 
     def initialize
       @env = {}
